685ef26b83a75ddf85f8fe5628e8be634e7f7502
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-01-14 17:16:07 +0900

    Added TransitionData

---
+++ code/TransitionData.py
@@ -0,0 +1,35 @@
+import astropy.io.fits
+import pandas
+import tqdm
+
+
+if __name__=="__main__":
+    database = astropy.io.fits.open("/Users/tanimoto/software/heasoft/heasoft-6.29/ftools/xstar/data/atdb.fits")
+    pointer  = database[1].data[0][0]
+    real     = database[2].data[0][0]
+    integer  = database[3].data[0][0]
+    string   = database[4].data[0][0]
+
+    Z            = []
+    Ion          = []
+    InitialLevel = []
+    FinalLevel   = []
+    Energy       = []
+    Strength     = []
+    Coefficient  = []
+
+    for i in tqdm.tqdm(range(len(pointer)//10)):
+        if pointer[10*i+1]==50 or pointer[10*i+1]==82:
+            if 0<float(real[pointer[10*i+7]-1]):
+                Z.append(integer[pointer[10*i+8]+1])
+                Ion.append(integer[pointer[10*i+8]+2])
+                InitialLevel.append(integer[pointer[10*i+8]+0])
+                FinalLevel.append(integer[pointer[10*i+8]-1])
+                Energy.append("{0:.4e}".format(12398/float(real[pointer[10*i+7]-1])))
+                Strength.append("{0:.4e}".format(real[pointer[10*i+7]+0]))
+                Coefficient.append("{0:.4e}".format(real[pointer[10*i+7]+1]))
+
+    df = pandas.DataFrame(data={"Z": Z, "Ion": Ion, "Initial Level": InitialLevel, "Final Level": FinalLevel, "Energy": Energy, "Oscillator Strength": Strength, "Einstein A Coefficient": Coefficient})
+    df = df.sort_values(["Ion", "Energy"])
+    df = df.reset_index(drop=True)
+    print(df)
